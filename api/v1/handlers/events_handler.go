6a389470ed0d6b1c34df12a80dc3541936020afa
Faiz <939656378@qq.com>
2025-06-11 19:43:10 +0800

    feat：update Events handler to accept dynamic cluster name

--- api/v1/handlers/events_handler.go
+++ api/v1/handlers/events_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/ciliverse/cilikube/internal/service"
+	"github.com/ciliverse/cilikube/pkg/k8s"
 	"github.com/ciliverse/cilikube/pkg/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -9,26 +10,38 @@ import (
 )
 
 type EventsHandler struct {
-	service *service.EventsService
+	service        *service.EventsService
+	clusterManager *k8s.ClusterManager
 }
 
-func NewEventsHandler(svc *service.EventsService) *EventsHandler {
+func NewEventsHandler(svc *service.EventsService, cm *k8s.ClusterManager) *EventsHandler {
 	return &EventsHandler{
-		service: svc,
+		service:        svc,
+		clusterManager: cm,
 	}
 }
 
 func (h *EventsHandler) ListEventsHandler(c *gin.Context) {
+	k8sClient, ok := k8s.GetK8sClientFromContext(c, h.clusterManager)
+	if !ok {
+		return
+	}
+
 	namespace := strings.TrimSpace(c.Param("namespace"))
 	if !utils.ValidateNamespace(namespace) {
 		respondError(c, http.StatusBadRequest, "无效的命名空间")
 		return
 	}
-	events := h.service.List(namespace)
+	events := h.service.List(k8sClient.Clientset, namespace)
 	respondSuccess(c, http.StatusOK, events)
 }
 
 func (h *EventsHandler) GetEventsHandler(c *gin.Context) {
+	k8sClient, ok := k8s.GetK8sClientFromContext(c, h.clusterManager)
+	if !ok {
+		return
+	}
+
 	namespace := strings.TrimSpace(c.Param("namespace"))
 	name := strings.TrimSpace(c.Param("name"))
 	if !utils.ValidateNamespace(namespace) || !utils.ValidateResourceName(name) {
@@ -39,6 +52,6 @@ func (h *EventsHandler) GetEventsHandler(c *gin.Context) {
 		respondError(c, http.StatusBadRequest, "事件名称不能为空")
 		return
 	}
-	event := h.service.Get(namespace, name)
+	event := h.service.Get(k8sClient.Clientset, namespace, name)
 	respondSuccess(c, http.StatusOK, event)
 }
